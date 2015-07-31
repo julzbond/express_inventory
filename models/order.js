84dc092c6c65560bba581caffa5f26b120466ddf
Julia Bond <julia@yogaed.com>
2015-07-30 19:20:55 -1000

    Changed config settings.

---
+++ models/order.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Order = sequelize.define('Order', {
+    name: DataTypes.TEXT,
+    quantity: DataTypes.INTEGER
+  }, {
+    underscored: true,
+    classMethods: {
+      associate: function(models) {
+        models.Order.belongsTo(models.Product, {foreignKey: 'product_id'});
+      }
+    }
+  });
+  return Order;
+};
